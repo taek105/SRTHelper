63a3b0beee665f3a04994a2c80e31eb166159e65
taek105 <ht5172@gmail.com>
2026-03-17 22:12:48 +0900

    fix(app.py): 개명역 반영

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 STATIONS = ["수서", "동탄", "평택지제", "천안아산", "오송", "대전",
-            "김천구미", "동대구", "신경주", "울산(통도사)", "부산",
+            "김천구미", "동대구", "경주", "울산(통도사)", "부산",
             "공주", "익산", "정읍", "광주송정", "나주", "목포"]
 
 @app.get("/", response_class=HTMLResponse)
